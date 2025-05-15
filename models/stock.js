3155317e20840b95c0873661bbaf7404f79f885c
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-05-15 20:17:01 +0700

    feat: add model relationships

--- models/stock.js
+++ models/stock.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Stock.belongsTo(models.Product, {
+        foreignKey: 'productId',
+        as: 'product'
+      });
     }
   }
   Stock.init({
--- models/transactions.js
+++ models/transactions.js
@@ -10,7 +10,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Transactions.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      });
+      Transactions.belongsToMany(models.Product, {
+        foreignKey: 'productId',
+        as: 'products'   
+      });
     }
   }
   Transactions.init({
