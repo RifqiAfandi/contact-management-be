4046addfa1bc4258da865da6bca80d6a1ba8ca5b
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-05-17 15:50:00 +0700

    feat: add auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,7 @@ const { authenticateToken, isAdmin } = require('../middlewares/authMiddleware');
 
 router.post('/users', authenticateToken, isAdmin, AuthController.createUser);
 router.post('/login', AuthController.login);
+router.get('/users', authenticateToken, isAdmin, AuthController.getAllUsers);
+router.get('/users/:id', authenticateToken, isAdmin, AuthController.getUserById);
 
 module.exports = router;
