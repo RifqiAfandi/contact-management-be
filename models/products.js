353262ce8eb635e4f320ce625cb36fd600b042ba
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-05-17 14:20:17 +0700

    refactor: add relationship product

--- models/products.js
+++ models/products.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Products.belongsTo(models.User, {
+        foreignKey: 'productId',
+        as: 'users',
+      });
     }
   }
   Products.init({
