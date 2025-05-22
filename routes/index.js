b6cc580a9e173e079b2f4c3320be007cda884020
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-05-22 15:31:44 +0700

    refactor: rename file routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authRoutes = require('./authRoutes');
-const productRoutes = require('./productRoutes');
+const authRoute = require("./authRoute");
+const productRoute = require("./productRoute");
+const inventoryRoute = require("./inventoryRoute");
 
-router.use('/auth', authRoutes);
-router.use('/products', productRoutes);
+router.use("/auth", authRoute);
+router.use("/products", productRoute);
+router.use("/inventory", inventoryRoute);
 
 module.exports = router;
