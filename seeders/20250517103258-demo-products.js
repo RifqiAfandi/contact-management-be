3dbb80e7379779ec79f755438c9bd0a0db6a24e0
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-05-27 14:47:59 +0700

    refactor: standardize formatting and update product URLs in demo products seeder

--- seeders/20250517103258-demo-products.js
+++ seeders/20250517103258-demo-products.js
@@ -1,40 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Products', [
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Products", [
       {
-        productName: 'Coffee Latte',
-        productUrl: 'https://example.com/coffee-latte.jpg',
+        productName: "Coffee Latte",
+        productUrl:
+          "https://ik.imagekit.io/RifqiAfandi/Coffe%20Latte.jfif?updatedAt=1748331977880",
         sellingPrice: 25000,
-        servingType: 'Hot/Ice',
+        servingType: "Hot/Ice",
         userId: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        productName: 'Cappuccino',
-        productUrl: 'https://example.com/cappuccino.jpg',
+        productName: "Cappuccino",
+        productUrl:
+          "https://ik.imagekit.io/RifqiAfandi/Cappucino.jfif?updatedAt=1748331977733",
         sellingPrice: 28000,
-        servingType: 'Hot/Ice',
+        servingType: "Hot/Ice",
         userId: 1,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        productName: 'Green Tea',
-        productUrl: 'https://example.com/green-tea.jpg',
+        productName: "Green Tea",
+        productUrl:
+          "https://ik.imagekit.io/RifqiAfandi/Green%20Tea.jfif?updatedAt=1748331977924",
         sellingPrice: 20000,
-        servingType: 'Hot/Ice',
+        servingType: "Hot/Ice",
         userId: 2,
         createdAt: new Date(),
-        updatedAt: new Date()
-      }
+        updatedAt: new Date(),
+      },
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Products', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Products", null, {});
+  },
 };
