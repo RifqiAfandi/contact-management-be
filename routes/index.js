e35315af21eb435e8e7f784efa7abeec51ba3dad
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-05-27 14:20:37 +0700

    feat: add transaction routes for handling transactions

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authRoute = require("./authRoute");
 const productRoute = require("./productRoute");
 const inventoryRoute = require("./inventoryRoute");
+const transactionRoute = require("./transactionRoute");
 
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
 router.use("/inventory", inventoryRoute);
+router.use("/transactions", transactionRoute);
 
 module.exports = router;
