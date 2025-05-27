e1f8d11a68f216f906bc11f47c8f957610776cd0
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-05-27 23:14:29 +0700

    refactor: remove authentication middleware from inventory routes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,29 +2,19 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const inventoryController = require("../controllers/inventoryController");
-const { authenticateToken, isAdmin } = require("../middlewares/authMiddleware");
 const { uploader } = require("../middlewares/uploader");
 
-router.get("/", authenticateToken, inventoryController.getAllInventory);
+router.get("/", inventoryController.getAllInventory);
 router.post(
   "/",
-  authenticateToken,
-  isAdmin,
   uploader.single("inventoryImg"),
   inventoryController.createInventory
 );
 router.put(
   "/:id",
-  authenticateToken,
-  isAdmin,
   uploader.single("inventoryImg"),
   inventoryController.updateInventory
 );
-router.delete(
-  "/:id",
-  authenticateToken,
-  isAdmin,
-  inventoryController.deleteInventory
-);
+router.delete("/:id", inventoryController.deleteInventory);
 
 module.exports = router;
